6c963a51402f4dcaa35bbbaf19d12dfb391d565d
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2023-02-28 18:49:31 +0200

    les1

---
+++ contextapp/src/App.js
@@ -0,0 +1,13 @@
+import BookList from "./Components/BookList";
+import NavBar from "./Components/NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <BookList />
+    </div>
+  );
+}
+
+export default App;
